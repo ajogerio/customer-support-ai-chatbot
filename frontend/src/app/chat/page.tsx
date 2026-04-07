6840521b61297f90362b7c2e9ccaa70843bde14e
ajogerio <aaogerio@up.edu.ph>
2026-04-07 23:46:59 +0800

    feat: transfer chat page logic to chat page

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,10 +1,32 @@
+'use client'
+
+import { useState } from 'react';
+import { Message, MESSAGE_SENDER } from '@/types/chat'
+import ChatInput  from '@/components/Chat/ChatInput'
 import Chat from '@/components/Chat/index'
 
 export default function ChatPage() {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [input, setInput] = useState<string>('');
+  
+  const sendMessage = () => {
+    if (input.trim() === '') return; // Do not send empty messages
+    setMessages([...messages, { text: input, sender: MESSAGE_SENDER.USER }]);
+    setInput('');
+
+    setTimeout(() => {
+      const aiMessage: Message = { text: 'Hello! How can I help you?', sender: MESSAGE_SENDER.AI };
+      setMessages((prev) => [...prev, aiMessage]);
+    }, 1000);
+  };
+
   return (
-    <div>
-      <h1>Customer Support Chat</h1>
-      <Chat />
+    <div className='flex flex-col h-screen'>
+      {/* Display Messages */}
+      <Chat messages={messages}/>
+
+      {/* Chat Input */}
+      <ChatInput input={input} setInput={setInput} sendMessage={sendMessage} />
     </div>
   )
 }
--- frontend/src/components/Chat/index.tsx
+++ frontend/src/components/Chat/index.tsx
@@ -1,34 +1,18 @@
 'use client';
 
-import { useState } from 'react';
-import { Message, MESSAGE_SENDER } from '@/types/chat'
-import ChatInput  from './ChatInput'
+import { Message } from '@/types/chat';
 import MessageBubble from './MessageBubble';
 
-export default function Chat() {
-  const [messages, setMessages] = useState<Message[]>([]);
-  const [input, setInput] = useState<string>('');
+interface ChatProps{
+  messages: Message[]
+}
 
-  const sendMessage = () => {
-    if (input.trim() === '') return; // Do not send empty messages
-    setMessages([...messages, { text: input, sender: MESSAGE_SENDER.USER }]);
-    setInput('');
-
-    setTimeout(() => {
-      const aiMessage: Message = { text: 'Hello! How can I help you?', sender: MESSAGE_SENDER.AI };
-      setMessages((prev) => [...prev, aiMessage]);
-    }, 1000);
-  };
-
+export default function Chat({ messages }: ChatProps) {
   return (
     <div>
-      {/* Display Messages */}
-      {messages.map((msg, index) => (
-        <MessageBubble key={index} message={msg} />
-      ))}
-      
-      {/* Chat Input */}
-      <ChatInput input={input} setInput={setInput} sendMessage={sendMessage} />
+        {messages.map((msg, index) => (
+          <MessageBubble key={index} message={msg} />
+        ))}
     </div>
   )
 }
