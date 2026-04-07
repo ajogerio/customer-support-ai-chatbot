4bdd9015d3401aea3976df87beeed20a9cae03e7
ajogerio <aaogerio@up.edu.ph>
2026-04-08 01:25:28 +0800

    feat: initialize the express app

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,8 @@
-import express, { Application, Request, Response } from 'express';
+import express, { type Application, type Request, type Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
