d617ed2ff272705075d41015cf826eb294044c3b
ajogerio <aaogerio@up.edu.ph>
2026-04-08 20:35:49 +0800

    chore: add eslint config and run lint

---
+++ backend/eslint.config.js
@@ -0,0 +1,25 @@
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+export default [
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        process: "readonly",
+        console: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      "no-console": "off", // Usually 'warn' in prod, but we need it for now
+      "semi": ["error", "always"],
+      "quotes": ["error", "single"],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
+    },
+  },
+];
